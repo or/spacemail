6cda8aa85ae565dcb02900d21322c98f43548d1d
Oliver Runge <oliver.runge@gmail.com>
2018-02-20 16:00:20 +0200

    Remove the noise stats indicator

--- bitbar-plugins/get-noise-stats.5m.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-values=$(notmuch search --output=messages --format=text tag:noise and date:1d.. | xargs notmuch show --format=json | jq '.[][0][0].headers.Date' | collect_buckets.py | tail -n 12)
-echo $values | awk '{print $3}' | spark
-echo "---"
-echo $values | awk '{print $1 " " $2 ":00" "   " $3}'
