4be21180caa7fc857aad9593aeac0166d64bf2c6
Oliver Runge <oliver.runge@gmail.com>
2023-01-21 18:23:16 +0100

    Silence mail display only, not the sync itself

--- xbar-plugins/sync-mail.5m.sh
+++ xbar-plugins/sync-mail.5m.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if test -f "/tmp/silence-mail"; then
-    echo "silenced | color=#707070"
-    exit 0
-fi
-
 script_path=$(readlink "$0")
 if [ -z "$script_path" ]; then
     script_path="$0"
@@ -12,4 +7,11 @@ fi
 
 cd "$(dirname "$script_path")"
 
-../bin/sync-mail
+
+if test -f "/tmp/silence-mail"; then
+    ../bin/sync-mail > /dev/null
+    echo "silenced | color=#707070"
+    exit 0
+else
+    ../bin/sync-mail
+fi
